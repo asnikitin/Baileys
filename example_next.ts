58fb3e9bbceef0924bb35b999ac3d926ebcb914a
drewoz <drewoz@protonmail.com>
2023-01-05 22:51:43 +0200

    mv to example_next.ts

--- example_next.ts
+++ example_next.ts
@@ -1,21 +1,30 @@
-import makeWASocket, { useMultiFileAuthState } from './lib';
+import makeWASocket, { fetchLatestBaileysVersion, makeInMemoryStore, useMultiFileAuthState } from './lib';
 import logger from './lib/Utils/logger';
 
 const myService = new (class MyService {
   connectionOpen = false;
   sock: ReturnType<typeof makeWASocket>;
+  store = makeInMemoryStore({ logger });
 
   constructor() {
     this.init();
   }
 
   async init() {
+    this.store?.readFromFile('./baileys_store_multi.json');
+    setInterval(() => {
+      this.store?.writeToFile('./baileys_store_multi.json');
+    }, 10_000);
+
+    const { version } = await fetchLatestBaileysVersion()
     const { state, saveCreds } = await useMultiFileAuthState('baileys_auth_info');
     this.sock = await makeWASocket({
+      version,
       auth: state,
       printQRInTerminal: true,
       logger,
-    })
+    });
+    this.store?.bind(this.sock.ev);
 
     this.sock.ev.on('connection.update', (event) => {
       if (event.connection === 'open') {
@@ -52,7 +61,8 @@ const myService = new (class MyService {
 
 async function run() {
   const sock = await myService.getSock();
-  await sock.setLabels(['foobar@s.whatsapp.net'], ['3', '5'], false);
+  await sock.setLabels(['foobar@s.whatsapp.net'], ['3', '5'], true);
+  logger.info(myService.store.getLabels('foobar@s.whatsapp.net'), 'getLabels');
 }
 
 run();
